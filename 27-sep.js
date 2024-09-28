eb3f25885bacb573f882b32ed73687fdc4aba4ec
UjjwalUpadhyay14 <142168540+UjjwalUpadhyay14@users.noreply.github.com>
2024-09-28 13:59:40 +0530

    codes for js 27,28 sep

---
+++ 27-sep.js
@@ -0,0 +1,84 @@
+// memory organization 
+
+// var a = 5;
+
+// var b = a;
+
+// b= 7;
+
+// console.log(b)
+// console.log(a)
+
+
+// var arr = [1,2,3,4,5,5.5,"Ujjwal"]
+
+// var arr2 =arr;
+
+
+// arr2.push(36)
+
+// console.log(arr)
+
+
+// datatypes 
+// primitive || non-primitive
+
+// String
+// number 
+// boolean
+// null
+// undefined
+// symbol 
+
+
+// non-premitive 
+// Array
+// var a = [1,2,3]
+// object 
+// var user ={
+//     name:"ankit",
+//     age:25,
+//     gmail:"jdsbcjdsfjsdn@gmail.com"
+// }
+// function 
+
+// function func (){
+//     console.log("hello")
+// }
+
+// func()
+
+
+
+// let add = function (){
+
+// }
+
+// add()
+
+
+
+
+
+// var maggi;
+
+// console.log(typeof(maggi))
+
+
+
+// String 
+
+
+// var a = "Yhb dkDfFERKNA "
+// console.log(a.toLowerCase())
+
+var a = new String("Ujjwal")
+
+console.log(a.charAt(3))
+
+console.log(a.indexOf('l'))
+
+
+
+
+
---
+++ 28-sep.js
@@ -0,0 +1,83 @@
+
+
+// var username = "   code_uj  "
+
+// console.log(str.substring(5,10))
+
+// console.log(username.trim())
+
+// console.log(username)
+
+// console.log(username.replace('uj','pj'))
+
+// var str = "this/is/a/class"
+
+// console.log(str.split('/'))
+
+
+
+
+
+// console.log(age.toString())
+
+
+var f = 54.7896321458;
+
+// console.log(f.toFixed(3))
+
+
+
+
+
+
+
+
+// Math object 
+
+// var age = -25;
+
+// console.log(Math.abs(age))
+
+
+
+// console.log(Math.sqrt())
+
+
+
+// console.log(Math.floor(Math.random()*10)+1)
+
+
+// var min = 1
+// var max = 6
+
+// console.log(Math.floor(Math.random()*(max-min+1)+ min))
+
+
+// var num_1 =56
+// var num_2 =78
+// var num_3 =46
+// var num_4 =36
+// var num_5 =86
+
+
+// var marks = [56,85,96,47,23]
+
+// console.log(Math.min(marks))
+
+
+
+// console.log(Math.max(num_1,num_2,num_3,num_4,num_5))
+
+
+
+
+
+// round 
+// floor 
+// ceil
+
+
+var num = 5.1;
+// console.log(Math.round(num))
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))

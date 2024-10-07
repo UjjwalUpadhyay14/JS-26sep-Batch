2540a9b5d3ac4beda693fd3b49fbceeb1fb36c58
UjjwalUpadhyay14 <142168540+UjjwalUpadhyay14@users.noreply.github.com>
2024-10-07 13:59:17 +0530

    code updates

---
+++ 07-oct.js
@@ -0,0 +1,116 @@
+// function print (){
+//     console.log("Hello")
+// }
+
+
+// print()
+
+
+
+// let sum = function (){
+//     console.log("function anon")
+// }
+
+
+// sum()
+
+
+
+// var sum = ()=> console.log("ujj")
+
+
+// (function print (){
+//     console.log("hello")
+// }) ();
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// console.log(sum(7,6))   
+
+
+// var cart = (...item)=>{
+//         console.log(item)
+// }
+
+
+// cart("bat","ball","glyves")
+
+
+// var fruit = ["apple","mango","tomato"]
+
+
+// function print(anyarray){
+//     console.log(anyarray[2])
+// }
+
+
+// print(fruit);
+
+
+// var obj = {
+//     name:"kaul",
+//     age:25
+// }
+
+
+// function print (aobj){
+//     console.log(aobj.name)
+// }
+
+
+// print(obj)
+
+
+// var user = {
+//     first_name:"sameer",
+//     last_name:"kaul",
+//     full_name: ()=>{
+//             console.log(`${this.firstname} ${this.last_name}`)
+//     }
+// }
+
+
+// console.log(user.full_name())
+
+
+
+// var first_name = "sam"
+// var last_name = "manekshaw"
+
+
+// console.log(`${first_name} ${last_name}`) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ login_signup.js
@@ -0,0 +1,41 @@
+// var user = {
+//     username:"code_uj",
+//     password:"Ujjwal@1403"
+// }
+
+
+// var username = "code_uj"
+// var password = "Ujjwal@1403"
+
+
+
+// if (username==user.username && password==user.password) {
+//     console.log(`${user.username} welcome in our site`)
+// } else {
+//     console.log("Invalid crecedentials")
+// }
+
+
+
+
+var cart = [
+
+    {
+        "product_name": "Apple",
+        "price": 100,
+    },
+    {
+        "product_name": "Banana",
+        "price": 50,
+    },
+    {
+        "product_name": "Orange",
+        "price": 70,
+    },
+    {
+        "product_name":"grapes",
+        "price": 80,
+    }
+]
+
+
